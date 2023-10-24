bec55e0899ab23266979db91983fc43fa0a01432
Muhammad Farabie <farabie024@gmail.com>
2023-10-24 23:48:34 +0700

    Variables

---
+++ Variables.php
@@ -0,0 +1,23 @@
+<?php
+
+$name = "Muhammad Farabie";
+$age = 23;
+
+echo 'Nama Saya: ';
+echo $name;
+echo "\n";
+echo 'Umur Saya: ';
+echo $age;
+
+
+echo "=================";
+echo "\n";
+$nama = "Farabie";
+$$name = "Keren";
+echo "\$name =";
+echo $name;
+echo "\n";
+echo "\$Farabie = ";
+echo $Farabie;
+echo "\n";
+?>
