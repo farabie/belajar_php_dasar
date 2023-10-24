941375c9d9b7b506c84c5985166701d045541c0e
Muhammad Farabie <farabie024@gmail.com>
2023-10-25 00:03:01 +0700

    Constant

---
+++ Constantant.php
@@ -0,0 +1,14 @@
+<?php 
+
+define("NAME_APP", "Kosanku");
+define("APP_VERSION", "1");
+
+
+//Best practice dalam constant yaitu uppercase
+echo "Name Aplication: ";
+echo NAME_APP;
+echo "\n";
+echo "Version Application: ";
+echo APP_VERSION;
+echo "\n";
+?>
