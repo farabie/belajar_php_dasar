dcf2a69fc32e30d2f4d9b45ec8ddc99cb270655a
Muhammad Farabie <farabie024@gmail.com>
2023-10-25 09:23:58 +0700

    Function

---
+++ Function.php
@@ -0,0 +1,22 @@
+<?php 
+
+
+//Function adalah block kode program yang akan berjalan saat kita panggil
+function sayHello() {
+    echo "Hello Function" . PHP_EOL;
+}
+
+sayHello();	
+
+//Lokasi function itu flexible bisa diubah didalam if ataupun dilainnya
+//Jadi kita bisa membuat function dimana saja termasuk juga di dalam if sekalipun
+$create = true;
+if($create){
+    function hi() {
+        echo "Hi Functio". PHP_EOL;
+    }
+}
+
+
+hi();
+?>
